9bbb2322c99274cba4d44932c6c958e421f03ac9
ahmedbeheiry <ahmed_beheiry@yahoo.com>
2020-05-22 05:31:07 +0200

    Fixes #3

--- projects/ngx-shimmer-loading/src/lib/ngx-shimmer-loading.module.ts
+++ projects/ngx-shimmer-loading/src/lib/ngx-shimmer-loading.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { NgxShimmerLoadingComponent } from './ngx-shimmer-loading.component';
 
 @NgModule({
   declarations: [NgxShimmerLoadingComponent],
-  imports: [BrowserModule],
+  imports: [CommonModule],
   exports: [NgxShimmerLoadingComponent]
 })
 export class NgxShimmerLoadingModule {}
